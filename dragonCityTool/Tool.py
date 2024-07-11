d0f5c62f0ff19b10dfa91b7afc0dd32b85514e78
Toanle <wibuneverdie676@gmail.com>
2024-07-11 21:37:55 +0700

    add dragontool file

--- dragonCityTool/Tool.py
+++ dragonCityTool/Tool.py
@@ -1,13 +1,13 @@
-from pynput.keyboard import Listener
+# from pynput.keyboard import Listener
 import pyautogui as auto
 from time import sleep
 
-def anonymous(key):
-    if str(key) == "Key.esc":
-        raise SystemExit(0)
-with Listener(on_press=anonymous) as user:
-    user.join()
-
+# def anonymous(key):
+#     if str(key) == "Key.esc":
+#         raise SystemExit(0)
+    
+# with Listener(on_press=anonymous) as user:
+#     user.join() 
 Path = {
     "breedMountain" : r"Img\dragonCity\breedMountain.png",
     "reBreed" : r"Img\dragonCity\reBreed.png",
@@ -21,90 +21,103 @@ Path = {
     "sellEgg" : r"Img\dragonCity\sellEgg.png",
     "sellDragon" : r"Img\dragonCity\sellDragon.png",
     "hatchMountain" : r"Img\dragonCity\hatchMountain.png",
-    "breedIcon" : r"Img\dragonCity\breedIcon.png",
+    "collectBreed" : r"Img\dragonCity\collectBreed.png",
+    "settingBtn" : r"Img\iconVsCode\settingBtn.png",
+    "xDeleteRed" : r"Img\dragonCity\xDeleteRed.png",
+    "teeraHabitat" : r"Img\dragonCity\teeraHabitat.png",
+    "teeraDragonInHabitat" : r"Img\dragonCity\teeraDragonInHabitat.png",
+    "placeEgg" : r"Img\dragonCity\placeEgg.png",
     
 }
 
-def find(objParam):
+auto.PAUSE = 1
+
+def find(objParam, stack):
     try:
-        objectPos = auto.locateCenterOnScreen(Path[objParam], confidence=0.7)
+        objectPos = auto.locateCenterOnScreen(Path[objParam], confidence=0.8)
         return objectPos
     except auto.ImageNotFoundException:
-        print("{} img not found".format(objParam))
-        sleep(2)
-        find(objParam)
-        
+        if stack < 3:    
+            print("{} img not found".format(objParam))
+            find(objParam, stack+1)
+        else:
+            return False
+def myPrompt(numOfLoops, myText, myTitle):
+    global quantity
+    if numOfLoops == 0:  
+        quantity = auto.prompt(text = myText, title = myTitle)
+    sleep(2)        
+    
+def action(object, duration):
+    objectPos = find("{}".format(object), 0)
+    auto.moveTo(objectPos, duration=duration)
+    auto.click()
+    
 class dragonTools:
-    def breedingTool(numOfLoops):
-        if numOfLoops == 0:
-            auto.prompt(text="Enter to start...", title="Start Breeding Tool")
-        sleep(2)
-        breedMountainPos = find("breedMountain")
-        auto.moveTo(breedMountainPos, duration=0.5)
-        auto.click(breedMountainPos)
-        sleep(1)
-        reBreedPos = find("reBreed")
-        auto.moveTo(reBreedPos, duration=0.5)
-        auto.click(reBreedPos)
-        sleep(1)
-        breedPos = find("breed")
-        auto.moveTo(breedPos, duration=0.5)
-        auto.click(breedPos)
-        sleep(2)
-        xDeleteRedBreedPos = find("xDeleteRedBreed")
-        auto.moveTo(xDeleteRedBreedPos, duration=0.5)
-        auto.click(xDeleteRedBreedPos)
-        sleep(1)
-        hatchMountainPos = find("hatchMountain")
-        auto.moveTo(hatchMountainPos, duration=0.5)
-        auto.click(hatchMountainPos)
-        sleep(1)
-        hatchEggPos = find("hatchEgg")
-        auto.moveTo(hatchEggPos, duration=0.5)
-        auto.click(hatchEggPos)
-        sleep(1)
-        sellDragonPos = find("sellDragon")
-        auto.moveTo(sellDragonPos, duration=0.5)
-        auto.click(sellDragonPos)
-        sleep(1)
-        sellEggPos = find("sellEgg")
-        auto.moveTo(sellEggPos, duration=0.5)
-        auto.click(sellEggPos)
-        sleep(5)
-        breedIconPos = find("breedIcon")
-        auto.moveTo(breedIconPos, duration=0.5)
-        auto.click(breedIconPos)
-        sleep(5)
+    def breedingTool(numOfLoops, myText, myTitle):
+        myPrompt(numOfLoops, myText, myTitle)
+        action("breedMountain", 0.5)
+        action("reBreed", 0.5)
+        action("breed", 0.5)
+        action("xDeleteRed", 0.5)
+        action("hatchMountain", 0.5)     
+        action("hatchEgg", 0.5)
+        action("sellDragon", 0.5)
+        action("sellEgg", 0.5)
+        action("collectBreed", 0.5)
+      
         
         return    
         
-    def collectFoodTool(numOfLoops):
-        global quantity
-        if numOfLoops == 0:  
-            quantity = int(auto.prompt(text="enter the num of foodHouse", title="Start collectFoodTool Tool"))
-        sleep(2)
-        foodHousePos = find("foodHouse")
-        auto.moveTo(foodHousePos, duration=0.5)
-        sleep(1)
-        auto.click(foodHousePos)
-        sleep(1)
-        reGrowPos = find("reGrow")
-        auto.moveTo(reGrowPos, duration=0.5)
-        sleep(1)
-        auto.click(reGrowPos)
-        sleep(30)
-        for _ in range(quantity):
-            foodIconPos = find("foodIcon")
+    def collectFoodTool(numOfLoops, myText, myTitle):
+        myPrompt(numOfLoops, myText, myTitle)
+        action("foodHouse", 0.5)
+        action("reGrow", 0.5)
+        sleep(31)
+        
+        foodIconPos = find("foodIcon", 0)
+        while foodIconPos:
             auto.moveTo(foodIconPos, duration=0.25)
-            auto.click(foodIconPos)  
-            sleep(0.25)
+            auto.click()  
+            sleep(0.5)
+            foodIconPos = find("foodIcon", 0)
                 
         return
               
-    def hatchEggTool(numOfLoops):
-        pass     
+    def hatchEggTool(numOfLoops, myText, myTitle):
+        myPrompt(numOfLoops, myText, myTitle)
+        for _ in range(int(quantity)):
+            action("hatchMountain", 0.5)
+            action("hatchEgg", 0.5)
+            action("placeEgg", 0.5)
+            action("teeraHabitat", 0.5)
+            action("teeraDragonInHabitat", 0.5)
+            action("sellDragon", 0.5)
+            action("sellEgg", 0.5)
+        for _ in range(int(quantity)):
+            action("hatchMountain", 0.5)
+            action("getEgg", 0.5)
+            action("goldStore", 0.5)
+            action("teeraDragon", 0.5)
+            action("buyTeeraEgg", 0.5)
+        sleep(17)    
+
+    def breedHatchTool(numOfLoops, myText, myTitle):
+        myPrompt(numOfLoops, myText, myTitle)
+        action("breedMountain", 0.5)
+        action("reBreed", 0.5)
+        action("breed", 0.5)
+        action("xDeleteRed", 0.5)
+        action("hatchMountain", 0.5)     
+        action("hatchEgg", 0.5)
+        action("placeEgg", 0.5)
+        action("teeraHabitat", 0.5)
+        action("teeraDragonInHabitat", 0.5)
+        action("sellDragon", 0.5)
+        action("sellEgg", 0.5)
+        
 for numOfLoops in range(50): 
     pass
-    # dragonTools.collectFoodTool(numOfLoops)yfy
-    # dragonTools.breedingTool(numOfLoops)
-    #
+    # dragonTools.collectFoodTool(numOfLoops, "Enter to start...", "COLLECT FOOD TOOL")
+    # dragonTools.breedingTool(numOfLoops, "Enter to start...", "BREEDING TOOL")
+    dragonTools.hatchEggTool(numOfLoops, "Enter the num of Eggs to start...", "HATCHING TOOL")
