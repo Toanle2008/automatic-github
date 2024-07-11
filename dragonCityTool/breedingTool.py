b867ab03d6f5971643800321e108cb8966900f70
Toanle <wibuneverdie676@gmail.com>
2024-07-11 07:26:22 +0700

    add . file

--- dragonCityTool/breedingTool.py
+++ dragonCityTool/breedingTool.py
@@ -1,24 +1,59 @@
+import pynput
 import pyautogui as auto
 from time import sleep
-
-auto.prompt(text="", title="Start Breeding Tool")
 
 Path = {
     "breedMountain" : r"Img\dragonCity\breedMountain.png",
     "reBreed" : r"Img\dragonCity\reBreed.png",
     "breed" : r"Img\dragonCity\breed.png",
-    "Xdelete_red" : r"Img\dragonCity\Xdelete_red.png",
-    "hatchMountain" : r"Img\dragonCity\hatchMountain.png",  
-    "hatchMountain" : r"Img\iconVsCode\user-icon.png",  
-    "hatchMountain" : r"Img\dragonCity\hatchMountain.png",  
+    "xDeleteRed" : r"Img\dragonCity\xdeleteRed.png",
+    "foodIcon" : r"Img\dragonCity\foodIcon.png",  
+    "foodHouse" : r"Img\dragonCity\foodHouse.png",  
+    "reGrow" : r"Img\dragonCity\reGrow.png",
+    
 }
-# print(auto.locateCenterOnScreen(r"Img\iconVsCode\user-icon.png", confidence=0.8))
+
 def find(objParam):
+    
     try:
-        objectPos = auto.locateCenterOnScreen(Path[objParam], confidence=0.8)
+        objectPos = auto.locateCenterOnScreen(Path[objParam], confidence=0.7)
         return objectPos
     except auto.ImageNotFoundException:
-        print("{objParam} not found")
+        print("{} img not found".format(objParam))
         sleep(2)
         find(objParam)
-print(find("hatchMountain")) 
+        
+class dragonTools:
+    def breedingTool():
+        # auto.prompt(text="", title="Start Breeding Tool")
+        sleep(1)
+
+        # print(auto.locateCenterOnScreen(r"Img\iconVsCode\user-icon.png", confidence=0.8))
+        # print(find("reBreed")) 
+    def collectFoodTool(numOfLoops):
+        global quantity
+        if numOfLoops == 0:  
+            quantity = int(auto.prompt(text="enter the num of foodHouse", title="Start collectFoodTool Tool"))
+        sleep(2)
+        foodHousePos = find("foodHouse")
+        auto.moveTo(foodHousePos, duration=0.5)
+        sleep(1)
+        auto.click(foodHousePos)
+        sleep(1)
+        reGrowPos = find("reGrow")
+        auto.moveTo(reGrowPos, duration=0.5)
+        sleep(1)
+        auto.click(reGrowPos)
+        sleep(30)
+        for _ in range(quantity):
+            foodIconPos = find("foodIcon")
+            auto.moveTo(foodIconPos, duration=0.25)
+            auto.click(foodIconPos)  
+            sleep(0.25)
+                
+        return
+         
+for numOfLoops in range(50): 
+    pass
+    dragonTools.collectFoodTool(numOfLoops)
+    # dragonTools.breedingTool(numOfLoops)
---
+++ dragonCityTool/collectFood.py
@@ -0,0 +1,6 @@
+import pynput
+import pyautogui as auto
+from time import sleep
+
+auto.prompt(text="", title="Start Breeding Tool")
+
